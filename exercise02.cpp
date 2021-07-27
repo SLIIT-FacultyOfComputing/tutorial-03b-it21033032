ae191c0099f76e03e79d297626e5a7bfc16ecac0
it21033032 <87374999+it21033032@users.noreply.github.com>
2021-07-27 08:51:13 +0000

    completed

--- exercise02.cpp
+++ exercise02.cpp
@@ -31,3 +31,12 @@ int main() {
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length){
+
+int answer;
+answer = height *  width * length;
+
+return answer;
+
+}
+
